d2b5b36266175083b6c4bd0b7d658e35ed7d4524
Paulo Rafael Barros de Aguiar <108704252+paulorbaguiar@users.noreply.github.com>
2023-11-24 10:40:54 -0300

    Update projetoSI_1.py

--- projetoSI_1.py
+++ projetoSI_1.py
@@ -1,4 +1,3 @@
-# listas com distancia direta de estação até as demais (tabela 1)
 est_dir_1 = [0.0, 10.0, 18.5, 24.8, 36.4, 38.8, 35.8, 25.4, 17.6, 9.1, 16.7, 27.3, 27.6, 29.8]
 est_dir_2 = [10.0, 0.0, 8.5, 14.8, 26.6, 29.1, 26.1, 17.3, 10.0, 3.5, 15.5, 20.9, 19.1, 21.8]
 est_dir_3 = [18.5, 8.5, 0.0, 6.3, 18.2, 20.6, 17.6, 13.6, 9.4, 10.3, 19.5, 19.1, 12.1, 16.6]
@@ -18,23 +17,23 @@ distancias_diretas = [est_dir_1, est_dir_2, est_dir_3, est_dir_4, est_dir_5, est
 
 
 # Dicionario contendo o número da estação linkado a uma lista de tuplas (num estação, cor linha, tempo real). (tabela 2)
-distancias_reais = {1 : [(2, "azul", 10)], 
-                    2 : [(1, "azul", 10), (3, "azul", 8.5), (10, "amarela", 3.5), (9, "amarela", 10)],
-                    3 : [(2, "azul", 8.5), (4, "azul", 6.3), (9, "vermelha", 9.4), (13, "vermelha", 18.7)],
-                    4 : [(3, "azul", 6.3), (5, "azul",  13.0), (8, "verde", 15.3), (13, "verde", 18.8)],
-                    5 : [(4, "azul",  13.0), (6, "azul", 3.0), (7, "amarela", 2.4), (8, "amarela", 30)],
-                    6 : [(5, "azul",  13.0)],
-                    7 : [(5, "amarela", 2.4)],
-                    8 : [(4, "verde", 15.3), (8, "amarela", 30), (9, "amarela", 9.6), (12, "verde", 6.4)],
-                    9 : [(2, "amarela", 10), (3, "vermelha", 9.4), (8, "amarela", 9.6), (11, "vermelha", 12.2)],
-                    10 : [(2, "amarela", 3.5)],
-                    11 : [(11, "vermelha", 12.2)],
-                    12 : [(12, "verde", 6.4)],
-                    13 : [(13, "vermelha", 18.7), (13, "verde", 18.8), (14, "verde", 5.1)],
-                    14 : [(14, "verde", 5.1)]}
+distancias_reais = {1: [(2, "azul", 10)], 
+    2: [(1, "azul", 10), (3, "azul", 8.5), (9, "amarela", 10), (10, "amarela", 3.5)],
+    3: [(2, "azul", 8.5), (4, "azul", 6.3), (9, "vermelha", 9.4), (13, "vermelha", 18.7)],
+    4: [(3, "azul", 6.3), (5, "azul",  13.0), (8, "verde", 15.3), (13, "verde", 12.8)],
+    5: [(4, "azul",  13.0), (6, "azul", 3.0), (7, "amarela", 2.4), (8, "amarela", 30)],
+    6: [(5, "azul",  3.0)],
+    7: [(5, "amarela", 2.4)],
+    8: [(4, "verde", 15.3), (5, "amarela", 30), (9, "amarela", 9.6), (12, "verde", 6.4)],
+    9: [(2, "amarela", 10), (3, "vermelha", 9.4), (8, "amarela", 9.6), (11, "vermelha", 12.2)],
+    10: [(2, "amarela", 3.5)],
+    11: [(9, "vermelha", 12.2)],
+    12: [(8, "verde", 6.4)],
+    13: [(3, "vermelha", 18.7), (4, "verde", 12.8), (14, "verde", 5.1)],
+    14: [(13, "verde", 5.1)]}
 
 
-# Algoritmo A* funcionando aparentemente. É preciso pesar a gora em como retornar o tempo e levar em consideração a baldeação
+# Algoritmo A* funcionando aparentemente. 
 def a_estrela(origem, origem_linha, destino, destino_linha):
     fronteiras_abertas = [] 
     fronteiras_fechadas = []
@@ -61,7 +60,7 @@ def a_estrela(origem, origem_linha, destino, destino_linha):
             print(f"A estação destino {destino} não esta na linha {destino_linha}. Observe o mapa e tente novamente")
             break
         
-        print(f'\nInteração {iteracao}:\nFronteiras abertas: {fronteiras_abertas}')
+        print(f'\nIteração {iteracao}:\nFronteiras abertas: {fronteiras_abertas}')
 
         fronteiras_abertas = sorted(fronteiras_abertas, key=lambda x: x[3])  # Ordenando as fronteiras de acordo com o índice 3 das tuplas
         
@@ -98,7 +97,10 @@ def a_estrela(origem, origem_linha, destino, destino_linha):
                 caminho_mais_rapido.append(fronteiras_fechadas[i])
             caminho_mais_rapido.reverse()  # Revertendo a lista para a primeira estação até a última
 
-            tempo_total_soma = sum(tempo_total) 
+            tempo_total_soma = sum(tempo_total)
+
+            if linha_atual != destino_linha: # Somar 4 caso a linha de destino seja diferente da linha de chegada.
+                tempo_total_soma += 4
 
             print("\nCaminho mais rápido até o seu destino é:")
             print(caminho_mais_rapido)
